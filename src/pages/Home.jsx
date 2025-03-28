4f6c1917e0fb03ebf7fa0bbf2d39a184a6099702
Ferdous Ahmed Himel <himibaba10@gmail.com>
2025-03-29 03:38:45 +0600

    Homepage design done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import berLogo from "../assets/images/Uber-Logo.wine.png";
+import { Link } from "react-router";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div
+      className={`bg-[url(/src/assets/images/traffic-light.jpg)] bg-cover bg-center min-h-screen w-full flex flex-col justify-between`}
+    >
+      <img
+        src={berLogo}
+        alt="Uber logo"
+        className="w-full max-w-28 ml-9 mt-9 invert"
+      />
+      <div className="bg-white p-5 pb-8">
+        <h1 className="text-3xl font-semibold mb-5">Get Started with Uber</h1>
+        <Link to={"/login"} className="primary-btn w-full">
+          <span>Continue</span>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
